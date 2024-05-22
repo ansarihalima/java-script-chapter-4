933b7bf3ac435b8fe39d4688cc42abc4a7625051
ansarihalima <halimasadiahalimasadia00@gmail.com>
2024-05-22 09:16:55 -0700

    chapter 4 questions solved

---
+++ index.js
@@ -0,0 +1,32 @@
+///////////////////////////QUESTION-1
+
+var variable1, variable2, variable3;
+
+///////////////////////////QUESTION-2
+
+////////////////Legal Variable Names:
+
+//myVariable
+//_myVariable
+//$myVariable
+//myVariable123
+//my_Variable
+
+
+////////////Illegal Variable Names:
+
+//123myVariable (Variable names cannot start with a number)
+//my Variable (Variable names cannot contain spaces)
+//my-Variable (Variable names cannot contain hyphens)
+//myVariable! (Variable names cannot contain special characters except underscore and dollar sign)
+//var (Reserved keywords like var cannot be used as variable names)
+
+///////////////////////////QUESTION-3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs.</p>");
+document.write("<p>For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign.</p>");
+document.write("<p>For example $name, _name or name</p>");
+document.write("<p>Variable names are case-sensitive.</p>");
+document.write("<p>Variable names should not be JS reserved keywords.</p>");
